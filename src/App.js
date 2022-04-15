4c60bb635907d51e0496055431bb63c407edd56d
emilykchow <emilykchow@outlook.com>
2022-04-14 23:23:45 -0400

    fetched api, display list of books, and added search bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import SearchBar from './components/SearchBar/SearchBar';
-import DisplayList from './components/DisplayList/DisplayList';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "../src/components/SearchBar/SearchBar.jsx"
 
 function App() {
-  // const [book, setBook] = useState({title: "", bookCover: "", author: "", publishedDate: ""});
-  // const [displayList, setDisplayList] = useState([]);
-  const [data, setData] = useState({});
+	const [data, setData] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [search, setSearch] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
 
-  useEffect(() => {
-   fetch("http://openlibrary.org/search.json?author=tolkien")
-   .then((res) => (res.json))
-   .then((data) => setData(data));
-   console.log(data)
-  }, [])
-  
+	useEffect(() => {
+		setLoading(true);
+		fetch("http://openlibrary.org/search.json?author=tolkien")
+			.then((response) => response.json())
+			.then((data) => setData(data))
+			.then(() => setLoading(false))
+			.catch(setError);
+	}, []);
 
-  return (
-    <div className="App">
-      <SearchBar />
-      <DisplayList />
+	if (loading) {
+		return <h1 style={{ textAlign: "center" }}>Loading...</h1>;
+	}
 
-    </div>
-  );
+	if (error) {
+		return <pre>{JSON.stringify(error, null, 2)}</pre>;
+	}
+
+	if (!data.docs) {
+		return null;
+	}
+
+	let x = data.docs;
+	let y = data.docs;
+
+	const searchHandler = (search) => {
+		setSearch(search);
+		if (search !== "") {
+			const newBooksList = y.filter((i) => {
+				return Object.values(i)
+					.join(" ")
+					.toLowerCase()
+					.includes(search.toLowerCase());
+			});
+			setSearchResults(newBooksList);
+		} else {
+			setSearchResults(y);
+		}
+	};
+
+	console.log(searchResults);
+
+	return (
+		<div className="container">
+			<SearchBar searchedItem={search} searchHandler={searchHandler} />
+			{search.length < 1 ? (
+				<ul className="list">
+					{x.map((item, i) => {
+						return (
+							<li key={i} className="list-item">
+								<i className="fa fa-book"></i>
+								&nbsp;
+								{item.title}
+							</li>
+						);
+					})}
+				</ul>
+			) : (
+				<ul className="list">
+					{searchResults.map((item, i) => {
+						return (
+							<li key={i} className="list-item">
+								<i className="fa fa-book"></i>
+								&nbsp;
+								{item.title}
+							</li>
+						);
+					})}
+				</ul>
+			)}
+		</div>
+	);
 }
-
 export default App;
